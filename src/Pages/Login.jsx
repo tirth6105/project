b97d41795c282766cbe0f4a0d1692f126a526096
tirth kalathiya <tirthkalathiya6105@gmail.com>
2024-01-01 13:28:25 +0530

    add json-server

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
+
+    let [email,setEmail] =useState("");
+    let [password,setPassword] =useState("");
+    let dispatch = useDispatch()
+
+    const handlelogin =async (email,password) => {
+      let user =  await axios.get(`http://localhost:8090/user?email=${email}&password=${password}`);
+
+      dispatch(Login(...user.data));
+    }
+
+    const handlesubmit = (e) =>{
+      e.preventDefault()
+      
+      handlelogin(email,password);
+      alert("Login successful!");
+    }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handlesubmit}>
+        <input type="email" value={email} placeholder='Email' onChange={(e)=>setEmail(e.target.value)} />
+        <input type="password" value={password} placeholder='Password' onChange={(e)=>setPassword(e.target.value)} />
+        <input type="submit" value='login' />
+      </form>
+    </div>
   )
 }
 
